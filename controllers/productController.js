3401c69cde1681d92271b223b18df873284eabe3
karasi001 <mishnefnara@gmail.com>
2025-07-01 02:23:31 +0300

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,7 @@ import pool from '../db.js';
 
 export const getProducts = async (req, res) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM Products');
+    const { rows } = await pool.query('SELECT * FROM products');
     res.json(rows);
   } catch (error) {
     console.error(error);
